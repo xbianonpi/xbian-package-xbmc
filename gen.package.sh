610770de17f903403a726ea158641c3490715621
Matus Kral <matuskral@me.com>
2013-05-15 15:43:41 +0200

    new gen.package.sh

--- gen.package.sh
+++ gen.package.sh
@@ -3,5 +3,5 @@
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
-fakeroot find ./content  | grep -v DEBIAN/ | sort | xargs md5sum > ./content/DEBIAN/md5sums 
+find ./content -print0 | xargs -0 -I{} sh -c ' test -d "{}" || echo $(cat "{}" | md5sum | ./fr.sh ) " {}" | grep -v DEBIAN/ | grep -v .DS_Store | sort -k3 ' > ./content/DEBIAN/md5sums
 fakeroot dpkg-deb -b ./content "${package}""${version}".deb
