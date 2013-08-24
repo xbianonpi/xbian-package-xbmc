ab089bbbbd5f1d42d6e762e4f620f3086ec30366
Matus Kral <matuskral@me.com>
2013-08-24 12:40:17 +0200

    new gen package script

--- gen.package.sh
+++ gen.package.sh
@@ -3,9 +3,12 @@
 rm_size() {
 	cat ./content/DEBIAN/control | grep -v "Installed-Size:" > ./content/DEBIAN/control.new
 	mv ./content/DEBIAN/control.new ./content/DEBIAN/control
+	rm -f ./size.txt
 }
 
 str='strip'
+strargs='--strip-unneeded'
+tot=0
 
 if [ "$(uname -m)" != 'armv6l' ]; then
     arm-linux-gnueabihf-strip > /dev/null 2>&1
@@ -15,14 +18,13 @@ fi
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
-#for f in $(ls ./content/etc/default/*); do cp $f $f.default; done
-
 # calculate size dynamically. remove first any entry, then add the actual 
 rm_size
 
 cd content
-[ -z "$str" ] || find ./ -type f -print0 |xargs --null $str 2>/dev/null
-printf "Installed-Size: %d\n" $(du -s ./ | awk '{print $1}') >> ./DEBIAN/control
+rm -fr ./tmp/*
+find ./ -type f -print0 | xargs -0 -L1 printf "%s\n" | while read f; do [ -z "$str" ] || $str $strargs "$f" 2>/dev/null; s=$(stat -c %s "$f"); tot=$((tot + (s/1024)+1)); echo $tot > ../size.txt; done
+printf "Installed-Size: %u\n" $(cat ../size.txt) >> ./DEBIAN/control
 find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P\0' | sort -z| xargs --null md5sum > DEBIAN/md5sums
 cd ..
 fakeroot dpkg-deb -b ./content "${package}""${version}".deb
