4a658b2d8295bff62449cb63140a42ea7abad233
Koenkk <koenkanters94@gmail.com>
2013-09-06 08:08:01 +0000

    Update XBMC

---
+++ content/usr/local/lib/xbmc/xbmc-config.cmake
@@ -0,0 +1,5 @@
+SET(XBMC_INCLUDE_DIR /usr/local/include)
+LIST(APPEND CMAKE_MODULE_PATH /usr/local/lib/xbmc)
+ADD_DEFINITIONS(-DTARGET_POSIX -DTARGET_LINUX -D_LINUX -D_ARMEL -DTARGET_RASPBERRY_PI)
+
+include(xbmc-addon-helpers)
