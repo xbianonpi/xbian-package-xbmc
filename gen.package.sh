320b11729778b9a0d8fea6991d814a69b6652f95
Matus Kral <matuskral@me.com>
2014-05-25 16:52:23 +0200

    update to control & helper files - added libnfs1 as dependency (https://github.com/xbianonpi/xbian/issues/543) - better handling of old Database during +C attr set (https://github.com/xbianonpi/xbian/issues/539#issuecomment-43282231)

--- gen.package.sh
+++ gen.package.sh
@@ -10,7 +10,7 @@ str='strip'
 strargs=''
 tot=0
 
-if [ "$(uname -m)" != 'armv6l' ]; then
+if ! dpkg-architecture -iarmhf; then
     arm-linux-gnueabihf-strip > /dev/null 2>&1
     [ $? -eq '127' ] && { echo "please install binutils-arm-linux-gnueabihf"; str=''; true; } || str='arm-linux-gnueabihf-strip'
 fi
@@ -23,7 +23,8 @@ rm_size
 
 cd content
 [ -d ./tmp ] && mv ./tmp ..
-find ./ -type f -print0 | xargs -0 -L1 printf "%s\n" | while read f; do [ -z "$str" ] || $str $strargs "$f" 2>/dev/null; s=$(stat -c %s "$f"); tot=$((tot + (s/1024)+1)); echo $tot > ../size.txt; done
+find ./ -type f -print0 | xargs -0 $str $strargs 2>/dev/null
+tot=$(du -sb | awk '{print $1}'); tot=$((tot/1024)); echo $tot > ../size.txt
 printf "Installed-Size: %u\n" $(cat ../size.txt) >> ./DEBIAN/control
 find ./ -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P\0' | sort -z| xargs --null md5sum > DEBIAN/md5sums
 cd ..
@@ -31,4 +32,5 @@ fakeroot dpkg-deb -b ./content "${package}""${version}".deb
 [ -d ./tmp ] && mv ./tmp ./content
 # remove the size again, because on different filesystems du will return different size
 rm_size
-#rm -f ./content/etc/default/*.default
+
+sync
